efc8f0b6fca60ba7112f319446b30bce385c0d38
Sergey Batalov <echodiv@outlook.com>
2021-08-11 15:10:09 +0300

    move api from APIView to ViewSet

--- users_srv/api/urls.py
+++ users_srv/api/urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls import url
 
-from .views import UsersListView, UsersDetailView
+from .views import UsersListViewSet, UserDetailViewSet
 
 
 app_name = 'api'
 
 urlpatterns = [
-       url(r'^users/(?P<pk>[\w]+)/$', UsersDetailView.as_view(),
+       url(r'^users/(?P<pk>[\w]+)/$', UserDetailViewSet.as_view(
+           {
+               'get': 'retrieve',
+               'put': 'update',
+               'patch': 'partial_update',
+               'delete': 'destroy',
+           }
+       ),
            name='user_manage'),
-       url(r'^users/$', UsersListView.as_view(), name='user_list'),
+       url(r'^users/$', UsersListViewSet.as_view(
+           {
+               'get': 'list',
+               'post': 'create',
+           }
+       ), name='user_list'),
 ]
--- users_srv/api/views.py
+++ users_srv/api/views.py
@@ -1,98 +1,29 @@
-from typing import Union
+from rest_framework.viewsets import ModelViewSet
 
-from rest_framework.generics import ListAPIView
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.contrib.auth.models import User
-from rest_framework.request import Request
 
 from .serializers import UserSerializer
 
 
-class UsersListView(ListAPIView):
+class UsersListViewSet(ModelViewSet):
     """
-    Получение всех пользователей системы
+    Используется для:
+    - отображения всех аккаунтов в системе {'get':'list'}
+    - создания нового пользователя {'post':'create'}
     """
+
+    serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class UserDetailViewSet(ModelViewSet):
+    """
+    Используется для управления аккаунтами
+    - отображение данных об аккаунте {'get':'retrieve'}
+    - замены данных аккаунта на новые {'put':'update'}
+    - частичное обновление данных аккаунта {'patch':'partial_update'}
+    - удаление аккаунта из системы {'delete':'destroy'}
+    """
+
     serializer_class = UserSerializer
-
-    def post(self, request: Request) -> Response:
-        """
-        Создание нового пользователя
-        """
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response({'errors': serializer.errors})
-
-
-class UsersDetailView(APIView):
-    serializer_class = UserSerializer
-
-    @staticmethod
-    def _check_valid_id(pk: str) -> Union[Response, User]:
-        """
-        Проверка типа идентификатора и наличия
-        пользователя с таким идентификатором
-        """
-        try:
-            int(pk)
-            user = User.objects.get(id=pk)
-        except ValueError:
-            return Response({'error': 'invalid id value'}, status=402)
-        except User.DoesNotExist:
-            return Response(data='', status=404)
-        return user
-
-    def get(self, request: Request, pk: str) -> Response:
-        """
-        Получение данных о конкретном пользователе
-        """
-        user = self._check_valid_id(pk)
-        if isinstance(user, Response):
-            return user
-        serialized_user = self.serializer_class(user)
-        return Response(serialized_user.data)
-
-    def put(self, request: Request, pk: str) -> Response:
-        """
-        Замена данных о пользователе (если он существует)
-        """
-        user = self._check_valid_id(pk)
-        if isinstance(user, Response):
-            return user
-        serializer = self.serializer_class(instance=user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response({'errors': serializer.errors})
-
-    def patch(self, request: Request, pk: str) -> Response:
-        """
-        Обновление данных о пользователе
-        """
-        user = self._check_valid_id(pk)
-        if isinstance(user, Response):
-            return user
-        serializer = self.serializer_class(
-            instance=user, data=request.data, partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response({'errors': serializer.errors})
-
-    def delete(self, request: Request, pk: str) -> Response:
-        """
-        Удаление пользователя по идентификатору
-        """
-        if not request.user.is_staff:
-            return Response({'error': 'permission denied'}, status=403)
-
-        user = self._check_valid_id(pk)
-        if isinstance(user, Response):
-            return user
-
-        user.delete()
-        return Response(status=204)
+    queryset = User.objects.all()
